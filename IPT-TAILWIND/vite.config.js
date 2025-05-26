cb54180aca62a7083ebd9a6e8579008446762525
Harroun <haroldnorwood39@gmail.com>
2025-05-26 20:17:54 +0800

    error fix 5

--- IPT-TAILWIND/vite.config.js
+++ IPT-TAILWIND/vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "file:///opt/render/project/src/IPT-TAILWIND/node_modules/vite/dist/node/index.js";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
